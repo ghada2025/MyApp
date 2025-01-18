dfbd3257c2b7d8d7d37dcacb8d62416bd2058fff
ghada2025 <khenieneghada@gmail.com>
2025-01-18 10:19:57 +0100

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import cors from "cors"
+import "dotenv/config"
+import express from "express"
+import helmet from "helmet"
+const app = express()
+// middlewares
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+app.use(express.static("./static"))
+
+app.set("view engine", "pug")
+app.set("views", "./views")
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+app.get("/service", (req, res) => {
+    res.render("service")
+})
+app.get("/contact", (req, res) => {
+    res.render("contact")
+})
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port", process.env.PORT)
+})
